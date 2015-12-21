ac814795f2cee649a63f0ec616925fc16336c054
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-21 08:54:54 -0200

    adding server structure

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Olá mundo');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+http.listen(4000, function(){
+	console.log('listening on *:4000');
+});
